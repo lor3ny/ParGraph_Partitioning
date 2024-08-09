74bb7f55b85b4950aa745bcb0bb3ace612534b12
Lorenzo Piarulli <lorenzo.piarulli3000@gmail.com>
2024-08-09 16:25:56 +0200

    setup

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+using namespace std;
+
+// is it better to use a adjacency matrix and select a well known memory strategy or adjacency lists?
+
+int main(){
+
+    // Graph creation
+
+    // Graph coarsening
+
+    // Partition of the coarsed graph
+
+    // Graph uncoarsening
+
+    // Print results
+}
