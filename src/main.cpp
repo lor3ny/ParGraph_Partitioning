b63d7150fc38d9e809dbc50447f0ca16f9a337ae
Lorenzo Piarulli <lorenzo.piarulli3000@gmail.com>
2024-08-11 15:12:14 +0200

    Basic graph generator started

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <list>
 
 
 using namespace std;
@@ -16,4 +17,40 @@ int main(){
     // Graph uncoarsening
 
     // Print results
+}
+
+bool IsThere(list<int> collection, int elem){
+
+    // Find elem on collection
+
+    return false;
+}
+
+
+list<int> generateGraphs(int nodesCount, int maxOutedges){
+
+    // A dynamic structure is needed
+    
+    list<int> nodes[nodesCount]; //array of lists
+    for(int i=0; i<nodesCount; i++){
+
+        // nodes[i] = [];
+
+        for(int j = 0; i< (maxOutedges - nodes[i].size()); i++){
+            // Pick random integer between 0 and nodesCount
+            
+            int newEdge = random(0, nodesCount); // Use a library
+
+            if(newEdge == i){
+                continue;
+            } 
+
+            if(IsThere(nodes[i],newEdge)){
+                continue;
+            }
+
+            nodes[i].push_back(newEdge);
+            nodes[newEdge].push_back(i); 
+        }
+    }
 }
